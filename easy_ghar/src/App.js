c9cc5ec526479aa5f28bf322aca3c27398acea7f
hamzakhan.4u99@gmail.com@ <subhanakhtar@Subhans-MacBook-Air.local>
2022-11-17 19:33:55 +0500

    easy ghar

---
+++ easy_ghar/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <div className='divv'><inputmodal/></div>
+      <Footer/>
+    </div>
+  );
+}
+
+
+
+export default App;
